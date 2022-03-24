9b4cf78c77b0b00fab430c0f50b7579d491fe537
Celi <kyouryuu.garini-ikuze@hotmail.co.jp>
2022-03-24 21:07:00 +0900

    index完成

--- css/style.js
+++ css/style.js
@@ -1,7 +1,7 @@
 function toggleNav() {
     var body = document.body;
-    var hamburger = document.getElementById('js-openbtn');
-    var blackBg = document.getElementById('js-blackBg');
+    var hamburger = document.getElementById('js-sidebar__button');
+    var blackBg = document.getElementById('js-blackBack');
   
     hamburger.addEventListener('click', function() {
       body.classList.toggle('nav-open');
@@ -11,24 +11,3 @@ function toggleNav() {
     });
   }
   toggleNav();
-
-
-
-
-
-
-
-
-// $(".l-sidebar__openbtn").click(function () {//ボタンがクリックされたら
-// 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-//     $(".l-sidebar___list").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-//     $(".l-sidebar__cover").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-
-// });
-
-// $(".l-sidebar___list a").click(function () {//ナビゲーションのリンクがクリックされたら
-//     $(".l-sidebar__openbtn").removeClass('active');//ボタンの activeクラスを除去し
-//     $(".l-sidebar__list").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
-//     $(".l-sidebar__cover").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
-
-// });
