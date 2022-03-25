8033ae77af9b992218428dc46966b9519aaf91cc
Celi <kyouryuu.garini-ikuze@hotmail.co.jp>
2022-03-25 19:26:14 +0900

    index,マップの高さ調整

--- css/style.js
+++ css/style.js
@@ -1,13 +1,20 @@
-function toggleNav() {
-    var body = document.body;
-    var hamburger = document.getElementById('js-sidebar__button');
-    var blackBg = document.getElementById('js-blackBack');
-  
-    hamburger.addEventListener('click', function() {
-      body.classList.toggle('nav-open');
-    });
-    blackBg.addEventListener('click', function() {
-      body.classList.remove('nav-open');
-    });
+$(function() {
+  function toggleNav() {
+      var body = document.body;
+      var hamburger = document.getElementById('js-sidebar__button');
+      var blackBg = document.getElementById('js-blackBack');
+    
+      hamburger.addEventListener('click', function() {
+        body.classList.toggle('nav-open');
+      });
+      blackBg.addEventListener('click', function() {
+        body.classList.remove('nav-open');
+      });
   }
   toggleNav();
+
+  $(window).on('load resize', function() {
+    var heightSize = $('.abc').outerHeight();
+    $('.p-map').css('height', heightSize)
+  });
+});
