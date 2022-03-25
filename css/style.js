2002d604151b1beae4f9cef348e98f5150a2e75a
Celi <kyouryuu.garini-ikuze@hotmail.co.jp>
2022-03-25 22:16:12 +0900

    index,サイドバーのスクロール調整

--- css/style.js
+++ css/style.js
@@ -1,9 +1,9 @@
 $(function() {
+  
   function toggleNav() {
       var body = document.body;
       var hamburger = document.getElementById('js-sidebar__button');
       var blackBg = document.getElementById('js-blackBack');
-    
       hamburger.addEventListener('click', function() {
         body.classList.toggle('nav-open');
       });
@@ -13,7 +13,7 @@ $(function() {
   }
   toggleNav();
 
-  $(window).on('load resize', function() {
+  $(window).on('load resize click', function() {
     var heightSize = $('.abc').outerHeight();
     $('.p-map').css('height', heightSize)
   });
